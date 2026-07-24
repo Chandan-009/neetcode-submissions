ca3011107ea5ebb00be710f379a565f6e614b48d
Chandan Kalluri <115135382+Chandan-009@users.noreply.github.com>
2026-07-24 12:39:43 +0530

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        int maxWindowSize=0;
+        Map<Character,Integer> map=new HashMap<>();
+        int i=0,j=0;
+        char maxChar=s.charAt(0);
+        for(char c: s.toCharArray()) { 
+            map.put(c,map.getOrDefault(c,0)+1);
+            maxChar=getMaxChar(map);
+            while((j - i + 1)-map.get(maxChar)>k){
+               char left_most_char=s.charAt(i);
+                map.computeIfPresent(left_most_char,(key,value)->(value==1)? null:value-1);
+                maxChar=getMaxChar(map);
+                i++;
+            }
+            maxWindowSize=Math.max(maxWindowSize,j - i + 1);
+            j++;
+            
+        }
+        return maxWindowSize;
+        
+    }
+
+    public static char getMaxChar(Map<Character, Integer> map) {
+    char maxChar = ' ';
+    int maxCount = 0;
+    for(Character c: map.keySet()){
+        if(map.get(c)>maxCount){
+            maxCount=map.get(c);
+            maxChar=c;
+        }
+
+    }
+    return maxChar;
+   }
+}
